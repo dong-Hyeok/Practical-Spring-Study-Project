9c2be21dd60605d4841ff3209f8f3ec7c742397c
하동혁 <mimiga1@naver.com>
2024-06-23 23:55:54 +0900

    [feat] JPA, DB 연결 & 테스트 코드 작성

---
+++ jpashop/src/test/java/com/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.jpabook.jpashop;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+    @Test
+    @Transactional
+    void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUserName("memberA");
+
+        // when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        // then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
+    }
+
+    @Test
+    void find() {
+    }
+}
