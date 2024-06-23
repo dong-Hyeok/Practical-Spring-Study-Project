8074e46813dc65354580b79310b71770457c9378
하동혁 <mimiga1@naver.com>
2024-06-24 01:22:48 +0900

    [feat] P6spy 라이브러리 추가

--- jpashop/src/test/java/com/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/com/jpabook/jpashop/MemberRepositoryTest.java
@@ -5,15 +5,22 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class MemberRepositoryTest {
 
+    /**
+     * 참고
+     * EntityManager를 이용한 모든 데이터 변경은 항상 트랜젝션 안에서 이루어져야 한다.
+     */
+
     @Autowired
     MemberRepository memberRepository;
     @Test
     @Transactional
+    @Rollback(value = false)
     void testMember() throws Exception {
         // given
         Member member = new Member();
@@ -26,6 +33,9 @@ class MemberRepositoryTest {
         // then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
+
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("findMember == member :  " + (findMember == member));
     }
 
     @Test
